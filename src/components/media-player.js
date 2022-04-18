1fad7fc0af2d20ff9b094ecba72afcc66b84497e
John Crosby <john@crsby.com>
2022-04-18 14:51:52 -0400

    chore: added media-player component

--- src/components/media-player.js
+++ src/components/media-player.js
@@ -1,23 +1,38 @@
+import { useEffect, useRef } from 'react';
+
 const MediaPlayer = ({ sources }) => {
-  const filteredSources = sources.filter((source) => {
-    const acceptedPlaybacks = ["mp4Avc", "hlsCloud", "highBit"];
-    return acceptedPlaybacks.includes(source.playback);
-  });
-  const getType = (source) => {
-    if (source.playback === "hlsCloud") {
-      return "application/x-mpegURL";
-    }
-    if (["highBit", "mp4Avc"].includes(source.playback)) {
-      return "video/mp4";
-    }
-  };
-  return (
-    <video autoPlay={true} muted={true} controls={true}>
-      {filteredSources.map((source) => (
-        <source src={source.location} type={getType(source)} />
-      ))}
-    </video>
-  );
+    const videoRef = useRef();
+    const filteredSources = sources.filter((source) => {
+        const acceptedPlaybacks = ['mp4Avc', 'hlsCloud', 'highBit'];
+        return acceptedPlaybacks.includes(source.playback);
+    });
+    const sortedSources = filteredSources.sort((source) =>
+        source.playback === 'hlsCloud' ? -1 : 0,
+    );
+    const getType = (source) => {
+        if (source.playback === 'hlsCloud') {
+            return 'application/x-mpegURL';
+        }
+        if (['highBit', 'mp4Avc'].includes(source.playback)) {
+            return 'video/mp4';
+        }
+    };
+    useEffect(() => {
+        videoRef.current?.load();
+    }, [sources]);
+    return (
+        <video
+            ref={videoRef}
+            autoPlay={true}
+            muted={true}
+            controls={true}
+            style={{ width: '320px', height: '180px' }}
+        >
+            {sortedSources.map((source, index) => (
+                <source key={`source-${index}`} src={source.location} type={getType(source)} />
+            ))}
+        </video>
+    );
 };
 
 export default MediaPlayer;
